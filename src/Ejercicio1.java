b66f83d1c0008249f1175ea9f4aff19fb0cbcd34
juankiph <zini_peke@hotmail.com>
2015-11-06 17:32:10 +0100

    inicio del proyecto

---
+++ src/Ejercicio1.java
@@ -0,0 +1,10 @@
+import acm.program.*;
+import acm.graphics.*;
+public class Ejercicio1 extends GraphicsProgram {
+	public void run(){
+		GLabel etiqueta = new  GLabel("HOLA GITANO");
+		add (etiqueta,120,130);
+	}
+	
+
+}
