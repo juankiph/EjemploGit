ad946e7594dbe6dee9f866b148846a2c13f0d8f7
juankiph <zini_peke@hotmail.com>
2015-11-06 19:22:36 +0100

    se ha creado la piramide entera

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,10 +1,24 @@
 import acm.program.*;
 import acm.graphics.*;
 public class Ejercicio1 extends GraphicsProgram {
+	
 	public void run(){
-		GLabel etiqueta = new  GLabel("HOLA GITANO");
-		add (etiqueta,120,130);
+		setSize(800,600);
+		pintaPiramide();
 	}
 	
+public void pintaPiramide(){
+	int numLadrillosBase = 14;
+	int anchoLadrillo = 30;
+	int altoLadrillo = 12;
+	for (int h=0; h< numLadrillosBase; h++){
+	for (int i=0; i< numLadrillosBase-h ; i++){
+		GRect ladrillo = new GRect(anchoLadrillo, altoLadrillo);
+		add(ladrillo,anchoLadrillo*h/2  + anchoLadrillo * i,altoLadrillo * h);
+		pause(900);
+	}
+	
+}
 
+}	
 }
