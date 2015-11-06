17b54cf73b3d1f32e7113849913e0e5260db7061
juankiph <zini_peke@hotmail.com>
2015-11-06 17:40:31 +0100

    inicio del metodo de la piramide

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,10 +1,15 @@
 import acm.program.*;
 import acm.graphics.*;
 public class Ejercicio1 extends GraphicsProgram {
+	
 	public void run(){
-		GLabel etiqueta = new  GLabel("HOLA GITANO");
-		add (etiqueta,120,130);
+		setSize(800,600);
+		pintaPiramide();
 	}
 	
+public void pintaPiramide(){
+	
+}
 
+	
 }
